c049962495b419b0002b8eefe89b0f56bde013b6
SnowBlack1 <varvoux.aurelie@gmail.com>
2020-10-23 12:20:08 +0200

    Link BottomNav & Fragments in MainActivity

--- app/src/main/java/com/aureliev/go4lunch/MainActivity.java
+++ app/src/main/java/com/aureliev/go4lunch/MainActivity.java
@@ -1,14 +1,51 @@
 package com.aureliev.go4lunch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity  {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        //MapsViewFragment opens first when launch
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_activity_frame_layout,
+                new MapsViewFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.map_view_tab:
+                            selectedFragment = new MapsViewFragment();
+                            break;
+                        case R.id.list_view_tab:
+                            selectedFragment = new ListViewFragment();
+                            break;
+                        case R.id.workmates_tab:
+                            selectedFragment = new WorkmatesFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_activity_frame_layout,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
 }
---
+++ app/src/main/java/com/aureliev/go4lunch/MapsViewFragment.java
@@ -0,0 +1,18 @@
+package com.aureliev.go4lunch;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MapsViewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.maps_view_fragment,container,false);
+    }
+}
