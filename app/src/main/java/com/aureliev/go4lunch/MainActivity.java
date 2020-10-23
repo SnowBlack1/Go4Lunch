087d82dd1794f336329608a622c756bf64d0dd28
SnowBlack1 <varvoux.aurelie@gmail.com>
2020-10-23 14:24:47 +0200

    NavigationDrawer + link MainActivity ok

--- app/src/main/java/com/aureliev/go4lunch/MainActivity.java
+++ app/src/main/java/com/aureliev/go4lunch/MainActivity.java
@@ -1,16 +1,24 @@
 package com.aureliev.go4lunch;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private BottomNavigationView mBottomNavigationView;
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +31,12 @@ public class MainActivity extends AppCompatActivity {
         //MapsViewFragment opens first when launch
         getSupportFragmentManager().beginTransaction().replace(R.id.main_activity_frame_layout,
                 new MapsViewFragment()).commit();
+
+        configureToolbar();
+        configureDrawerLayout();
+        configureNavigationView();
     }
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -48,4 +61,50 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
+    private void configureToolbar() {
+        this.mToolbar = findViewById(R.id.main_activity_toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    private void configureDrawerLayout() {
+        this.mDrawerLayout = findViewById(R.id.main_activity_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.nav_drawer_open, R.string.nav_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    private void configureNavigationView() {
+        this.mNavigationView = findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_drawer_lunch:
+                break;
+            case R.id.nav_drawer_settings:
+                break;
+            case R.id.nav_drawer_logout:
+                break;
+            default:
+                break;
+        }
+        this.mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
+
+
